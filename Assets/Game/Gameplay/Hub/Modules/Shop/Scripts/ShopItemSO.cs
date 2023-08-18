e5cb8717894a0978792f330099267aa813ace4b8
Ignacio Fernandez Lemos <ignacio.f.l@hotmail.com>
2023-08-18 18:52:17 -0300

    refact(hub): create shop items

---
+++ Assets/Game/Gameplay/Hub/Modules/Shop/Scripts/ShopItemSO.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+using BlueGravity.Common.Currencies;
+
+[CreateAssetMenu(fileName = "shopitem_", menuName = "ScriptableObjects/Hub/Shop/Shop item")]
+public class ShopItemSO : ScriptableObject
+{
+    [Header("Main Configuration")]
+    [SerializeField] private ItemSO item = null;
+
+    [Header("Price Configuration")]
+    [SerializeField] private CurrencySO currencyType = null;
+    [SerializeField] private int price = 0;
+
+    private bool isPurchased = false;
+
+    public ItemSO Item { get => item; }
+    public CurrencySO CurrencyType { get => currencyType; }
+    public int Price { get => price; }
+    public bool IsPurchased { get => isPurchased; }
+
+    public void SetIsPurchased(bool status)
+    {
+        isPurchased = status;
+    }
+}
